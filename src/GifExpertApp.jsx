b63beb042cf285d0df4bd8cfd15772226e17198f
Galeid <galeid475@gmail.com>
2021-05-06 16:08:29 -0500

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import AddCategory from './components/AddCategory'
+import GifGrid from './components/GifGrid'
+
+const GifExpertApp = () => {
+
+   const [categories, setCategories] = useState(['Overwatch'])
+
+   return (
+      <>
+         <h2>GifExpertApp</h2>
+         <hr />
+
+         <AddCategory setCategories={ setCategories } />
+
+         <ol>
+            { categories.map( ( cat ) => (
+               <GifGrid key={ cat } category={ cat } />
+            )) }
+         </ol>
+      </>
+   )
+}
+
+export default GifExpertApp
